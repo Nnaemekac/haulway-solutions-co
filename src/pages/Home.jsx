ebe2ec72128f4dc62ee75fe46fe82a8ba1207ea2
Nnaemekac <nnaemekachimezie5@gmail.com>
2024-01-30 15:24:21 -0800

    ---

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -75,7 +75,7 @@ const Home = () => {
       
       <div className="header-section xui-row xui-pt-9 xui-lg-pb-8 xui-pb-5 xui-container">
         <div id="welcome" className="hero-content xui-lg-col-6  xui-font-w-bold"> 
-          <p className="xui-font-w-normal xui-text-dc-underline" onClick={togglePalette}>Click to customize your palette</p>       
+          <p className="xui-font-w-normal xui-text-dc-underline xui-cursor-pointer" onClick={togglePalette}>Click to customize your palette</p>       
           <h1 className="xui-lg-font-sz-350 xui-font-sz-250 font-spaceGrotesk xui-mt-half">From Ideas to Successful Companies</h1>
           <p className="xui-font-sz-80 xui-opacity-8 xui-line-height-1-half xui-w-fluid-80 xui-mt-half">We believe that entrepreneurship can unlock human potential and make the world a better place.</p>
           <HashLink className="xui-bdr-rad-half xui-text-dc-none xui-font-sz-80 secondary xui-text-white xui-py-1 xui-d-inline-block xui-mt-1 xui-px-2" smooth to='#companies'>Our companies</HashLink>
