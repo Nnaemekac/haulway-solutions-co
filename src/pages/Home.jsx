8bbb52daee4d27e4085f5c13f74e9e5acabec28d
Nnaemekac <nnaemekachimezie5@gmail.com>
2024-03-29 19:36:49 -0700

    ---

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -167,90 +167,24 @@ const Home = () => {
         </div>
         
       </div>
-      <div className="header-section xui-d-block xui-lg-d-none mobile-header-section xui-pt-9 xui-lg-pb-5 xui-pb-3 xui-container">
-        <div className="xui-d-flex xui-pos-relative xui-z-index-1">
-        <div className="header-middle xui-z-index--1 xui-h-500" style={{backgroundImage: `url(https://img.freepik.com/free-photo/multicultural-businesswomen-group-meeting_1157-47735.jpg?t=st=1709381715~exp=1709385315~hmac=7292fab0cad514a184adc3b28336b9dde6731be3d127550f2ecc2a5beea8bed2&w=740)`}}>
-              <div className="xui-overlay-1 xui-h-fluid-100 xui-pos-relative xui-bdr-rad-1">
-                <h3 class="xui-font-sz-300 xui-font-9 mobile-break break xui-font-w-500 xui-line-height-2-half">
-                  <span data-aos="fade-right" data-aos-duration="800" class="word-background xui-w-300">Welcome to</span>
-                  <span data-aos="fade-right" data-aos-delay="400" data-aos-duration="800" class="word-background xui-w-250">Grascope</span>
-                  
-                </h3>
-                <div data-aos="fade-in" data-aos-duration="800" className='xui-d-inline-flex header-component xui-flex-ai-center solution xui-pos-absolute xui-text-white glass-bg xui-py-half xui-bdr-rad-1 xui-px-half'>
-                    <div className="xui-w-40 xui-h-40 xui-bdr-rad-circle xui-bg-white xui-d-flex xui-flex-ai-center xui-flex-jc-center">
-                      <Send size={15} color={"#000"} strokeWidth={1.6} />
-                    </div>
-                    <h3 className="xui-font-sz-70 font-spaceGrotesk xui-ml-half">E-commerce Solutions</h3>
-                </div>
-                <div data-aos="fade-in" data-aos-delay="400" data-aos-duration="800" className='header-component xui-d-inline-flex xui-flex-ai-center solution solution2 xui-pos-absolute xui-text-white glass-bg xui-py-half xui-bdr-rad-1 xui-px-half'>
-                    <div className="xui-w-40 xui-h-40 xui-bdr-rad-circle xui-bg-white xui-d-flex xui-flex-ai-center xui-flex-jc-center">
-                      <Send size={15} color={"#000"} strokeWidth={1.6} />
-                    </div>
-                    <h3 className="xui-font-sz-70 font-spaceGrotesk xui-ml-half">Agrotech Solutions</h3>
-                </div>
-                {/* <div className="xui-d-flex xui-flex-ai-center xui-pos-absolute mini-companies-holder">
-                    <img data-aos="fade-in" data-aos-delay="600" data-aos-duration="800" className="mini-companies xui-w-50 xui-h-50 xui-bdr-rad-circle" src="https://img.freepik.com/free-photo/ecofriendly-fresh-market-produce_482257-76147.jpg?size=626&ext=jpg&ga=GA1.2.3493108.1707066373&semt=ais" alt="" />
-                    <img data-aos="fade-in" data-aos-delay="800" data-aos-duration="800" className="mini-companies xui-w-50 xui-h-50 xui-bdr-rad-circle" src="https://img.freepik.com/free-photo/ecofriendly-fresh-market-produce_482257-76147.jpg?size=626&ext=jpg&ga=GA1.2.3493108.1707066373&semt=ais" alt="" />
-                    <img data-aos="fade-in" data-aos-delay="900" data-aos-duration="800" className="mini-companies xui-w-50 xui-h-50 xui-bdr-rad-circle" src="https://img.freepik.com/free-photo/ecofriendly-fresh-market-produce_482257-76147.jpg?size=626&ext=jpg&ga=GA1.2.3493108.1707066373&semt=ais" alt="" />
-                </div> */}
-              </div>
+      <div style={{backgroundImage: `url(https://img.freepik.com/free-photo/focused-call-center-operator-typing-laptop_74855-4213.jpg?t=st=1710770736~exp=1710774336~hmac=f6dc562d54e5ee791ae472dda0b7033342102e55e50b8dbbe7489a199fa6734e&w=740)`}} className="header-section xui-bg-sz-cover xui-bg-pos-center xui-d-block xui-lg-d-none mobile-header-section">
+        <div className="xui-overlay-1 xui-container xui-h-fluid-100 xui-pos-relative xui-pt-9 xui-pb-7">
+            <img src={Grascope} className="xui-img-150 xui-bdr-rad-1" alt="" />
+            <h1 class="xui-font-sz-300 xui-font-9 xui-font-w-500 xui-line-height-2-half xui-mt-2">Welcome to Grascope</h1>
+            <p data-aos="fade-down" data-aos-delay="800" data-aos-duration="800" className="xui-font-sz-80 xui-mt-1 xui-opacity-6 xui-line-height-1-half xui-w-fluid-100 xui-mt-half">Grascope Industries Ltd is not just a leader in innovation; we're also pioneers in entrepreneurship, owning and operating a diverse portfolio of companies across various industries.</p>
+            <HashLink smooth to='#services' data-aos="fade-down" data-aos-delay="900" data-aos-duration="800" className="btn xui-mt-1 xui-text-dc-none xui-text-black">
+              <span className="xui-w-40 xui-h-40 xui-bdr-rad-circle secondary xui-d-flex xui-flex-jc-center xui-flex-ai-center">
+                <ArrowUpRight size="19" color="#FFF"/>
+              </span>
+              <p className="xui-font-w-bold xui-font-sz-90 xui-text-white">Get started</p>
+            </HashLink>
             
+            {/* <div className="xui-d-flex xui-flex-ai-center xui-pos-absolute mini-companies-holder">
+                <img data-aos="fade-in" data-aos-delay="600" data-aos-duration="800" className="mini-companies xui-w-50 xui-h-50 xui-bdr-rad-circle" src="https://img.freepik.com/free-photo/ecofriendly-fresh-market-produce_482257-76147.jpg?size=626&ext=jpg&ga=GA1.2.3493108.1707066373&semt=ais" alt="" />
+                <img data-aos="fade-in" data-aos-delay="800" data-aos-duration="800" className="mini-companies xui-w-50 xui-h-50 xui-bdr-rad-circle" src="https://img.freepik.com/free-photo/ecofriendly-fresh-market-produce_482257-76147.jpg?size=626&ext=jpg&ga=GA1.2.3493108.1707066373&semt=ais" alt="" />
+                <img data-aos="fade-in" data-aos-delay="900" data-aos-duration="800" className="mini-companies xui-w-50 xui-h-50 xui-bdr-rad-circle" src="https://img.freepik.com/free-photo/ecofriendly-fresh-market-produce_482257-76147.jpg?size=626&ext=jpg&ga=GA1.2.3493108.1707066373&semt=ais" alt="" />
+            </div> */}
           </div>
-            <div className="header-left xui-pos-relative">
-              {/* <h3 class="xui-font-sz-350 break xui-font-w-500 xui-line-height-2-half">
-                <span data-aos="fade-right" data-aos-duration="800" class="word-background xui-w-500">Welcome to</span>
-                <span data-aos="fade-right" data-aos-delay="400" data-aos-duration="800" class="word-background xui-w-400">Grascope</span>
-                
-              </h3> */}
-            
-              <div className="header-left-contents xui-pr-1">
-                <p data-aos="fade-down" data-aos-delay="800" data-aos-duration="800" className="xui-font-sz-80 xui-opacity-6 xui-line-height-1-half xui-w-fluid-100 xui-mt-half">Grascope Industries Ltd is not just a leader in innovation; we're also pioneers in entrepreneurship, owning and operating a diverse portfolio of companies across various industries.</p>
-                <HashLink smooth to='#services' data-aos="fade-down" data-aos-delay="900" data-aos-duration="800" className="btn xui-mt-1 xui-text-dc-none xui-text-black">
-                  <span className="xui-w-40 xui-h-40 xui-bdr-rad-circle secondary xui-d-flex xui-flex-jc-center xui-flex-ai-center">
-                    <ArrowUpRight size="19" color="#FFF"/>
-                  </span>
-                  <p className="xui-font-w-bold xui-font-sz-90">Get started</p>
-                </HashLink>
-              </div>
-            </div>
-            
-            <div data-aos="fade-down" data-aos-delay="400" data-aos-duration="800" className="header-right">
-            <div data-aos="fade-down" data-aos-delay="400" data-aos-duration="800" className="header-right xui-pl-1">
-              {/* <img className="xui-h-300 xui-w-fluid-100 xui-bdr-rad-1" src="https://img.freepik.com/free-photo/focused-call-center-operator-typing-laptop_74855-4213.jpg?t=st=1710770736~exp=1710774336~hmac=f6dc562d54e5ee791ae472dda0b7033342102e55e50b8dbbe7489a199fa6734e&w=740" alt="" /> */}
-              <Swiper
-              autoplay={true}
-              effect='fade'
-              modules={[Autoplay, EffectFade]}
-              slidesPerView={1}
-              onSwiper={(swiper) => console.log(swiper)}
-              className="header-swiper"
-              
-              >
-                  <SwiperSlide>
-                    <img className="xui-h-300 xui-w-fluid-100 xui-bdr-rad-1" src="https://img.freepik.com/free-photo/focused-call-center-operator-typing-laptop_74855-4213.jpg?t=st=1710770736~exp=1710774336~hmac=f6dc562d54e5ee791ae472dda0b7033342102e55e50b8dbbe7489a199fa6734e&w=740" alt="" />
-                  </SwiperSlide>
-                  <SwiperSlide>
-                    <img className="xui-h-300 xui-w-fluid-100 xui-bdr-rad-1" src="https://img.freepik.com/free-photo/woman-having-online-meeting-work_23-2148940760.jpg?t=st=1711391963~exp=1711395563~hmac=60cd78578dcdc0d78d713d0bcc6ceeb74ae27fa7ee346eb6913237d659647712&w=740" alt="" />
-                  </SwiperSlide>
-                  <SwiperSlide>
-                    <img className="xui-h-300 xui-w-fluid-100 xui-bdr-rad-1" src="https://img.freepik.com/free-photo/medium-shot-bored-people-working_23-2150697629.jpg?t=st=1711392003~exp=1711395603~hmac=40a93e5161140824147aee6afff3ef99fd14980fd6acde93d031f3af0feef439&w=740" alt="" />
-                  </SwiperSlide>
-              </Swiper>
-              <div>
-                <div className="xui-d-flex xui-mt-1">
-                  <span className="xui-w-60 xui-h-60 xui-bdr-rad-circle secondary xui-d-flex xui-flex-jc-center xui-flex-ai-center">
-                    <ArrowUpRight size="19" color="#FFF"/>
-                  </span>
-                  <div className="xui-pl-1" style={{"width": "calc(100% - 60px)"}}>
-                    <p className="xui-font-w-bold xui-font-sz-90">Custom software solutions</p>
-                    <p className="xui-font-sz-80 xui-opacity-6 xui-line-height-1-half xui-w-fluid-100 xui-mt-half">Harness the power of tailored technology with our custom software solutions.</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            </div>
-        </div>
         
       </div>
       <div id="services" className="xui-lg-pt-5 xui-pt-2 xui-lg-pb-3 xui-pb-2 xui-container">
